6fae68d6ad1f877805ed0f5e3000a693a84b4c64
Melvin W <melvinhw@me.com>
2022-11-11 09:41:52 +0000

    Initial Commit

---
+++ AnimationDemo/AnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationDemoApp.swift
+//  AnimationDemo
+//
+//  Created by M W on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
